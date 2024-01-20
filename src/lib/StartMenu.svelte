62c0d07f7e2f155ae13382364f1e29b49fdff506
gawgua <taisaodeoduoc@gmail.com>
2024-01-20 19:48:07 +0700

    initial change (2 pages)

---
+++ src/lib/StartMenu.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	
+	const dispatch = createEventDispatcher();
+
+	function click() {
+		dispatch("click");
+	}
+</script>
+
+<button type="button" class="start" on:click={click}>Ấn đi</button>
+
+<style>
+	button {
+		font-size: 3em;
+		padding: 30px 80px;
+	}
+</style>
