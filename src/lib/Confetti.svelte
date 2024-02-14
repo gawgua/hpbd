e49a1f33f8ac85a36e050690e53e91e37afe426f
gawgua <taisaodeoduoc@gmail.com>
2024-02-14 20:09:18 +0700

    add confetti

---
+++ src/lib/Confetti.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let confettiContainer: HTMLDivElement;
+	let confettiClass: string;
+	onMount(() => {
+		setInterval(() => {
+			if (!confettiClass) confettiClass = confettiContainer.childNodes[0].className;
+			let confetti: HTMLDivElement = document.createElement("div");
+			confetti.style.left = Math.random() * window.innerWidth + "px";
+			confetti.textContent = Math.random() > 0.5 ? "🎊" : "🎉";
+			confetti.className = confettiClass;
+
+			if (confettiContainer.childNodes.length > 30) confettiContainer.childNodes[0].remove();
+			confettiContainer.appendChild(confetti);
+		}, 400);
+	});
+</script>
+
+<div id="confetti-container" bind:this={confettiContainer}>
+	<div class="confetti">🎉</div>
+</div>
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap");
+
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		overflow: hidden;
+		font-family: "Poppins", sans-serif;
+	}
+
+	#confetti-container {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+
+	.confetti {
+		position: absolute;
+		top: 0;
+		font-size: 6rem;
+		animation: confetti-fall linear 5s infinite;
+	}
+	@keyframes confetti-fall {
+		0% {
+			transform: translateY(-100%) rotate(0);
+		}
+		100% {
+			transform: translateY(100vh) rotate(180deg);
+		}
+	}
+</style>
--- src/lib/ImagesGrid.svelte
+++ src/lib/ImagesGrid.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 	import { createEventDispatcher } from "svelte";
+	import Confetti from "./Confetti.svelte";
 
 	const dispatch = createEventDispatcher();
 
@@ -30,10 +31,8 @@
 		(entries, observer) => {
 			entries.forEach((e) => {
 				let img: HTMLElement = e.target;
-				if (e.intersectionRatio > 0.55) 
-					img.style.opacity = "1";
-				else
-					img.style.opacity = `${e.intersectionRatio}`;
+				if (e.intersectionRatio > (window.innerWidth >= window.innerHeight ? 0.3 : 0.55)) img.style.opacity = "1";
+				else img.style.opacity = `${e.intersectionRatio}`;
 			});
 		},
 		{ threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] }
@@ -49,6 +48,9 @@
 		<link rel="preload" as="image" href={url} />
 	{/each}
 </svelte:head>
+<div class="confetti">
+	<Confetti />
+</div>
 <h1 class="rainbow_text_animated">Gương mặt của cô nàng xinh đẹp có ngày sinh nhật hôm nay UwU</h1>
 <div class="grid">
 	<div class="grid-item">
@@ -63,17 +65,24 @@
 	</div>
 </div>
 <div class="but">
-	<button on:click={() => dispatch("next")}>Có 1 lời nhắn cho bạn</button>
+	<button on:click={() => dispatch("next")}>Có <span style="color: red; font-size:large">1</span> lời nhắn cho bạn</button>
 </div>
 
 <style>
+	.confetti {
+		position: relative;
+		z-index: 1;
+	}
+
 	.but {
+		position: relative;
 		margin: 20px 0;
 		display: flex;
 		justify-content: center;
+		z-index: 2;
 	}
-
 	button {
+		background-color: #f0afa3;
 		text-align: center;
 		font-size: medium;
 		padding: 15px 30px;
@@ -86,21 +95,20 @@
 		-webkit-text-stroke-width: 1px;
 		padding: 50px 0px;
 		margin: 0;
+		z-index: 1;
 	}
 	.rainbow_text_animated {
-		background: linear-gradient(to right, #6666ff, #0099ff, #00ff00, #ff3399, #6666ff);
+		background: linear-gradient(to right, #ff5837, #ff8e78, #fc3a14);
+		background-size: 400% 100%;
 		background-clip: text;
 		color: transparent;
-		animation: rainbow_animation 6s ease-in-out infinite;
-		background-size: 400% 100%;
+		animation: rainbow_animation ease-in-out 3s infinite;
 	}
-
 	@keyframes rainbow_animation {
 		0%,
 		100% {
 			background-position: 0 0;
 		}
-
 		50% {
 			background-position: 100% 0;
 		}
@@ -110,13 +118,12 @@
 		display: flex;
 		flex-wrap: wrap;
 		padding: 0 5vw;
+		z-index: 1;
 	}
-
 	.grid-item {
 		flex: 25%;
 		padding: 0 4px;
 	}
-
 	.grid-item .image {
 		margin-top: 10px;
 		vertical-align: middle;
